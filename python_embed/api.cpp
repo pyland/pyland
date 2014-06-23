09a57a0e3f2dbac7e6f17ebabb7f501aa5091d0f
Joshua Landau <joshua@landau.ws>
2014-06-23 14:58:37 +0100

    Working on API tests for Python-C++ interface

--- python_embed/api.cpp
+++ python_embed/api.cpp
@@ -1,44 +1,50 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <boost/python.hpp>
 #include "api.h"
+
+namespace py = boost::python;
 
 Vec2D::Vec2D(int x, int y): x(x), y(y) {};
 
 Vec2D Vec2D::operator+(Vec2D other) {
-	return Vec2D(x + other.x, y + other.y);
+    return Vec2D(x + other.x, y + other.y);
 }
 
 void Vec2D::operator+=(Vec2D& other) {
-	x += other.x;
-	y += other.y;
+    x += other.x;
+    y += other.y;
 }
 
 std::ostream& operator<<(std::ostream& out, Vec2D& in) {
-	return out << in.to_string();
+    return out << in.to_string();
 }
 
 std::string Vec2D::to_string() {
-	std::ostringstream stringStream;
-	stringStream << "(" << x << " " << y << ")";
-	return stringStream.str();
+    std::ostringstream stringStream;
+    stringStream << "(" << x << " " << y << ")";
+    return stringStream.str();
 }
 
 
 
 Player::Player(Vec2D start, Direction direction, std::string name):
-	position(start), direction(direction), name(name), script("") {}
+    position(start), direction(direction), name(name), script("") {}
 
 void Player::move(Vec2D by) {
-	position += by;
+    position += by;
 }
 
 void Player::monologue() {
-	std::cout << "I am " << name << "and I am standing at " << position << "!" << std::endl;
+    std::cout << "I am " << name << " and I am standing at " << position << "!" << std::endl;
 }
 
-// TODO:
-void Player::run_script() {}
+void Player::run_script() {
+    script(py::ptr(this));
+}
 
-void Player::give_script(std::string in) {}
+void Player::give_script(py::api::object callable) {
+    script = callable;
+}
 
--- python_embed/interpreter.cpp
+++ python_embed/interpreter.cpp
@@ -7,33 +7,47 @@
 namespace py = boost::python;
 
 int main(int argc, char **argv) {
-	Py_Initialize();
+    Py_Initialize();
 
-	// Get the current directory and add it to Python's sys.path
-	boost::filesystem::path workingDir = boost::filesystem::absolute("./").normalize();
-	py::api::object sys_paths = py::import("sys").attr("path");
-	sys_paths.attr("append")(py::str(workingDir.string()));
+    // Get the current directory and add it to Python's sys.path
+    boost::filesystem::path workingDir = boost::filesystem::absolute("./").normalize();
+    py::api::object sys_paths = py::import("sys").attr("path");
+    sys_paths.attr("append")(py::str(workingDir.string()));
 
-	try {
-		py::api::object main_module = py::import("__main__");
-		py::api::object main_namespace = main_module.attr("__dict__");
+    try {
+        py::api::object main_module = py::import("__main__");
+        py::api::object main_namespace = main_module.attr("__dict__");
 
-		py::api::object wrapper_module = py::import("wrapper_functions");
-		main_module.attr("wrapper_functions") = wrapper_module;
+        py::api::object wrapper_module = py::import("wrapper_functions");
+        main_module.attr("wrapper_functions") = wrapper_module;
 
-		Player my_player = Player(Vec2D(90, 102), Direction::UP, "John");
-		main_module.attr("preplayer") = py::object(my_player);
+        Player my_player = Player(Vec2D(90, 102), Direction::UP, "John");
+        py::object preplayer = py::object(my_player);
 
-		py::exec(
-			"player = wrapper_functions.Player(2, 6)\n"
-			"player.speak_position()\n"
-			"player.move(10, -3)\n"
-			"player.speak_position()\n"
-			"preplayer.speak_position()\n",
-			main_namespace
-		);
-	}
-	catch (boost::python::error_already_set) {
-		PyErr_Print();
-	}
+        main_module.attr("preplayer") = preplayer;
+        main_module.attr("Vec2D") = py::object(Vec2D(0, 0)).attr("__class__");
+
+        py::exec(
+            "preplayer.monologue()\n"
+            "preplayer.move(Vec2D(10, 14))\n"
+            "preplayer.monologue()\n"
+
+            "def script(player):\n"
+            "    for _ in range(1000):\n"
+            "        player.move(Vec2D(1, 0))\n"
+            "    player.monologue()\n"
+
+            "preplayer.give_script(script)\n"
+            "preplayer.run_script()\n"
+            "preplayer.monologue()\n"
+            ,
+            main_namespace
+        );
+
+        my_player.monologue();
+        preplayer.attr("monologue")();
+    }
+    catch (boost::python::error_already_set) {
+        PyErr_Print();
+    }
 }
