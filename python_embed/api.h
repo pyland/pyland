5532b247615779d859fb9f4633fa311703d6e2f2
Heidi-ann <hh360@cam.ac.uk>
2014-06-26 10:18:37 +0100

    documenting the Vec2D class

--- python_embed/api.h
+++ python_embed/api.h
@@ -3,13 +3,38 @@
 
 enum class Direction {UP, DOWN, LEFT, RIGHT};
 
+
 class Vec2D {
     public:
         int x;
         int y;
+
+        /**
+         *  Vec2D constructor
+         *  @param x x coordinate
+         *  @param y y coordinate
+         *  @return instance of Vec2D
+         */
         Vec2D(int x, int y);
+
+        /**
+         *  addition of Vec2Ds, not mutating either vector
+         *  @param other the other coordinate being added
+         *  @return new instance of Vec2D
+         */
         Vec2D operator+(Vec2D other);
+
+        /**
+         *  addition of Vec2Ds, mutating the vector its called on (not other)
+         *  @param other the other coordinate being added
+         *  @return void 
+         */
         void operator+=(Vec2D other);
+
+        /**
+         *  conversion of Vec2D to string
+         *  @return string representation of Vec2D
+         */
         std::string to_string();
 };
 
