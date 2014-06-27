5e90b03df73d60e1112c49a950e4958aee8757ae
Joshua Landau <joshua@landau.ws>
2014-06-27 10:01:07 +0100

    Add forcing

--- python_embed/test_timeout.cpp
+++ python_embed/test_timeout.cpp
@@ -3,12 +3,22 @@
 #include <mutex>
 #include <string>
 #include <thread>
+#include <ctime>
 
 std::timed_mutex lock;
 
 void waitsometime() {
 	std::cout << 1 << std::endl;
-	lock.try_lock_for(std::chrono::milliseconds(10000));
+
+	auto end = std::chrono::system_clock::now() + std::chrono::milliseconds(10000);
+	while (end > std::chrono::system_clock::now()) {
+		lock.try_lock_for(
+			end - std::chrono::system_clock::now()
+			+ std::chrono::milliseconds(10)
+		);
+	}
+
+
 	std::cout << 2 << std::endl;
 }
 
