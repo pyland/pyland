9a0477eda7db771c41bd4cba4ff108c4809def84
Joshua Landau <joshua@landau.ws>
2014-06-27 10:06:54 +0100

    Made less busy

--- python_embed/test_timeout.cpp
+++ python_embed/test_timeout.cpp
@@ -11,13 +11,12 @@ void waitsometime() {
 	std::cout << 1 << std::endl;
 
 	auto end = std::chrono::system_clock::now() + std::chrono::milliseconds(10000);
-	while (end > std::chrono::system_clock::now()) {
-		lock.try_lock_for(
-			end - std::chrono::system_clock::now()
-			+ std::chrono::milliseconds(10)
-		);
-	}
 
+	while (true) {
+		lock.try_lock_for(end - std::chrono::system_clock::now());
+		if (end >= std::chrono::system_clock::now()) { break; }
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
 
 	std::cout << 2 << std::endl;
 }
