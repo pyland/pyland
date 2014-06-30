7078dec3a80b3940d53ed56ef7b70adedafa10bf
Joshua Landau <joshua@landau.ws>
2014-06-30 11:21:19 +0100

    Spaces to tabs

--- python_embed/locks.cpp
+++ python_embed/locks.cpp
@@ -5,43 +5,43 @@
 int GIL::i = 0;
 
 GIL::GIL() {
-	inst = i;
-	++i;
-	print_debug << inst << " Aquiring GIL" << std::endl;
-	gilstate = PyGILState_Ensure();
-	// PyEval_AcquireLock();
-	print_debug << inst << " GIL aquired: " << std::endl;
+    inst = i;
+    ++i;
+    print_debug << inst << " Aquiring GIL" << std::endl;
+    gilstate = PyGILState_Ensure();
+    // PyEval_AcquireLock();
+    print_debug << inst << " GIL aquired: " << std::endl;
 }
 
 GIL::~GIL() {
-	print_debug << inst << " Releasing GIL" << std::endl;
-	// PyEval_ReleaseLock();
-	PyGILState_Release(gilstate);
-	print_debug << inst << " GIL released" << std::endl;
+    print_debug << inst << " Releasing GIL" << std::endl;
+    // PyEval_ReleaseLock();
+    PyGILState_Release(gilstate);
+    print_debug << inst << " GIL released" << std::endl;
 }
 
 
 ThreadGIL::ThreadGIL(ThreadState &threadstate) {
-	PyEval_RestoreThread(threadstate.get_threadstate());
+    PyEval_RestoreThread(threadstate.get_threadstate());
 }
 
 ThreadGIL::~ThreadGIL() {
-	PyEval_SaveThread();
+    PyEval_SaveThread();
 }
 
 
 ThreadState::ThreadState(PyInterpreterState *interpreter_state) {
-	threadstate = PyThreadState_New(interpreter_state);
+    threadstate = PyThreadState_New(interpreter_state);
 }
 
-ThreadState::~ThreadState() {	
-	{
-		GIL lock_gil;
-    	PyThreadState_Clear(threadstate);
+ThreadState::~ThreadState() {   
+    {
+        GIL lock_gil;
+        PyThreadState_Clear(threadstate);
     }
     PyThreadState_Delete(threadstate);
 }
 
 PyThreadState *ThreadState::get_threadstate() {
-	return threadstate;
+    return threadstate;
 }
