ac1892bedebfee0ba2deec47d0b03807ef46a1af
Heidi-ann <hh360@cam.ac.uk>
2014-07-01 17:37:12 +0100

    adding script with auto quit

--- src/sprite-test.sh
+++ src/sprite-test.sh
@@ -4,4 +4,7 @@ for i in {1..50}
 do
    echo "trying $i sprites"
    sed -i bak -E "/const int num_objects = /s/[0-9]+/$i/g" main.cpp
+   sed -i bak -E "/const int num_objects = /s/[0-9]+/$i/g" python_embed/interpreter.cpp
+   COMPILER=g++-4.7 PYTHON_VERSION=3.2 LIBBOOST_PYTHON=boost_python-py32 make
+   timeout -k 20 20 ./main.bin 
 done
