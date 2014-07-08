4f11663d7762fa13fa652a348ba8063a341cf225
Heidi-ann <hh360@cam.ac.uk>
2014-07-08 16:41:46 +0100

    adding *argv

--- src/python_embed/py_wrapper.py
+++ src/python_embed/py_wrapper.py
@@ -1,7 +1,8 @@
 import time
 import inspect
 
-def fun_wrapper (func, delay, args):
+def fun_wrapper (func, delay, args, kwargs):
+	print (func.__name__)
 	if delay:
 		time.sleep(0.1)
 	if len(args) == 0:
@@ -10,14 +11,14 @@ def fun_wrapper (func, delay, args):
 		return func (args[0])
 
 
-def move(x):
-    return fun_wrapper(player.move, True, (x,))
+def move(*args, **kwargs):
+    return fun_wrapper(player.move, True, args, kwargs)
     
-def monologue():
-    return fun_wrapper(player.monologue, False, ())
+def monologue(*args, **kwargs):
+    return fun_wrapper(player.monologue, False, args, kwargs)
 
-def walkable(x):
-	return fun_wrapper(player.walkable, False, (x,))
+def walkable(*args, **kwargs):
+	return fun_wrapper(player.walkable, False, args, kwargs)
 
 north, south, east, west = Vec2D(0, 1), Vec2D(0, -1), Vec2D(1, 0), Vec2D(-1, 0)    
 def script(player):
