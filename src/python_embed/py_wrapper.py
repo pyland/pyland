27cf71924bbd5c463efb144f4289cb6a102088fa
Heidi-ann <hh360@cam.ac.uk>
2014-07-08 16:14:37 +0100

    adding function for wrapping python

--- src/python_embed/py_wrapper.py
+++ src/python_embed/py_wrapper.py
@@ -1,15 +1,23 @@
 import time
+import inspect
+
+def fun_wrapper (func, delay, args):
+	if delay:
+		time.sleep(0.1)
+	if len(args) == 0:
+		return func
+	elif len(args) == 1:
+		return func (args[0])
+
 
 def move(x):
-    player.move(x)
-    time.sleep(0.1)
-    return True
+    return fun_wrapper(player.move, True, (x,))
     
 def monologue():
-    player.monologue()
+    return fun_wrapper(player.monologue, False, ())
 
 def walkable(x):
-	return (player.walkable(x))
+	return fun_wrapper(player.walkable, False, (x,))
 
 north, south, east, west = Vec2D(0, 1), Vec2D(0, -1), Vec2D(1, 0), Vec2D(-1, 0)    
 def script(player):
