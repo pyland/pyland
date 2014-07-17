5dd05ca20d07529dae7b786350c685c980b79111
Heidi-ann <hh360@cam.ac.uk>
2014-07-17 10:50:56 +0100

    adding challenge 1 and associated map

---
+++ src/challenges/challenge1.cpp
@@ -0,0 +1,9 @@
+void dialogue (std::string name, std:string text_to_user) {
+	std:cout << name << " : " << text_to_user << std::endl
+} 
+
+void run_challenge() {
+	dialogue (
+		"Welcome to Project Zgyote, My name is PI
+		")
+}
