6fa5e865be3e1d383c00a2ce2b1779a35efe0a0a
Joshua Landau <joshua@landau.ws>
2014-07-27 01:04:59 +0100

    Improved printing for multiline strings.

--- src/challenges/challenge.cpp
+++ src/challenges/challenge.cpp
@@ -7,5 +7,5 @@ Challenge::Challenge(std::string editor): editor(editor) {}
 
 void Challenge::dialogue(std::string name, std::string text_to_user) {
     // TODO: Use rendered fonts
-    std::cout << name << "  says: " << text_to_user << std::endl;
+    std::cout << name << "  says:\n" << text_to_user << std::endl;
 } 
