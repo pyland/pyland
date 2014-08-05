84bff6c6f5dee305c339b216a2905849f00c7f1e
Heidi-ann <hh360@cam.ac.uk>
2014-08-05 12:18:58 +0100

    removing extra debugging output from tile_to_pixel in map

--- src/map.cpp
+++ src/map.cpp
@@ -555,15 +555,11 @@ Vec2D Map::tile_to_pixel(Vec2D tile_location) {
 }
 
 Vec2D Map::tile_to_pixel(std::pair<double,double> tile_location) {
-    VLOG(2) << "incoming tiles" << tile_location.first << " " << tile_location.second;
     double scale(TILESET_ELEMENT_SIZE*GLOBAL_SCALE);
 
     std::pair<double,double> results(tile_location.first * scale, tile_location.second * scale);
-
-    VLOG(2) << "incoming get_display" << Map::get_display_x() << " " << Map::get_display_y();
     results.first = results.first - (Map::get_display_x() * scale) ;
     results.second = results.second - (Map::get_display_y() * scale) ;
-    VLOG(2) << "outgoing pixel" << results.first << " " << results.second;
     return Vec2D( (int)results.first, (int)results.second);
 }    
 
