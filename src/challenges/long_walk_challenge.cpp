764f690be1d9ff31664fa6ca445a33897d6acf8e
Heidi-ann <hh360@cam.ac.uk>
2014-08-07 11:49:05 +0100

    merging with ben's changes

--- src/challenges/long_walk_challenge.cpp
+++ src/challenges/long_walk_challenge.cpp
@@ -1,6 +1,7 @@
 #include <glog/logging.h>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -12,6 +13,9 @@
 #include "long_walk_challenge.hpp"
 #include "engine_api.hpp"
 #include "map_viewer.hpp"
+#include "object_manager.hpp"
+#include "map_object.hpp"
+#include "walkability.hpp"
 
 //TODO: later this will be fetched from the map
 std::map<std::string, std::vector<Vec2D>> targets = {
@@ -48,6 +52,13 @@ std::map<std::string, std::vector<Vec2D>> targets = {
 
 LongWalkChallenge::LongWalkChallenge(InputManager *input_manager): Challenge(input_manager) {
     auto *map = Engine::get_map_viewer()->get_map();
+
+    // set up a test of map_object
+    std::shared_ptr<MapObject> test_chest = std::make_shared<MapObject>(10, 15, "test chest",5);
+    ObjectManager::get_instance().add_object(test_chest);
+    auto chest_id = test_chest->get_id();
+    LOG(INFO) << "created test_chest with id: " << chest_id;
+    test_chest->set_walkability(Walkability::BLOCKED);
 
     // Set up blocking walls
     for (auto wall_location : targets.at("wall:path:medium")) {
