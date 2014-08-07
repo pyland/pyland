c7064c264e6be001b8f8e2cef4b1640b330e32de
Heidi-ann <hh360@cam.ac.uk>
2014-08-07 14:56:45 +0100

    removing demo code

--- src/challenges/long_walk_challenge.cpp
+++ src/challenges/long_walk_challenge.cpp
@@ -54,13 +54,13 @@ LongWalkChallenge::LongWalkChallenge(InputManager *input_manager): Challenge(inp
     auto *map = Engine::get_map_viewer()->get_map();
 
     //set up a test of map_object
-    std::shared_ptr<MapObject> test_chest = std::make_shared<MapObject>(10, 15, "test chest",5);
-    ObjectManager::get_instance().add_object(test_chest);
-    auto chest_id = test_chest->get_id();
-    LOG(INFO) << "created test_chest with id: " << chest_id;
-    map->add_map_object(chest_id);
-    test_chest->set_walkability(Walkability::WALKABLE);
-    test_chest->set_tile_sheet_id(119);
+    // std::shared_ptr<MapObject> test_chest = std::make_shared<MapObject>(10, 15, "test chest",5);
+    // ObjectManager::get_instance().add_object(test_chest);
+    // auto chest_id = test_chest->get_id();
+    // LOG(INFO) << "created test_chest with id: " << chest_id;
+    // map->add_map_object(chest_id);
+    // test_chest->set_walkability(Walkability::WALKABLE);
+    // test_chest->set_renderable(false);
 
     // Set up blocking walls
     for (auto wall_location : targets.at("wall:path:medium")) {
