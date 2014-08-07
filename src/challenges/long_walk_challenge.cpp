a2f497f6633f860c59dd48154a3c41929f836ef8
Heidi-ann <hh360@cam.ac.uk>
2014-08-07 13:08:47 +0100

    removing test wall

--- src/challenges/long_walk_challenge.cpp
+++ src/challenges/long_walk_challenge.cpp
@@ -53,13 +53,14 @@ std::map<std::string, std::vector<Vec2D>> targets = {
 LongWalkChallenge::LongWalkChallenge(InputManager *input_manager): Challenge(input_manager) {
     auto *map = Engine::get_map_viewer()->get_map();
 
-    // set up a test of map_object
-    std::shared_ptr<MapObject> test_chest = std::make_shared<MapObject>(10, 15, "test chest",5);
-    ObjectManager::get_instance().add_object(test_chest);
-    auto chest_id = test_chest->get_id();
-    LOG(INFO) << "created test_chest with id: " << chest_id;
-    map->add_map_object(chest_id);
-    test_chest->set_walkability(Walkability::BLOCKED);
+    //set up a test of map_object
+    // std::shared_ptr<MapObject> test_chest = std::make_shared<MapObject>(10, 15, "test chest",5);
+    // ObjectManager::get_instance().add_object(test_chest);
+    // auto chest_id = test_chest->get_id();
+    // LOG(INFO) << "created test_chest with id: " << chest_id;
+    // map->add_map_object(chest_id);
+    // test_chest->set_walkability(Walkability::WALKABLE);
+
 
     // Set up blocking walls
     for (auto wall_location : targets.at("wall:path:medium")) {
