2010e863bbf865c50e3ef9dd0704cf28276fb441
Heidi-ann <hh360@cam.ac.uk>
2014-08-08 16:09:44 +0100

    adding new create_pickupable method

--- src/challenge_helper.hpp
+++ src/challenge_helper.hpp
@@ -9,7 +9,12 @@ public:
 
     ///
     /// given an object, allow it to picked up and dropped off at specific locations
+    /// only one pick up and drop off is permitted
+    ///
     static void create_pickupable(Vec2D start_tile, Vec2D pickup_tile, 
                            Vec2D finish_tile, Vec2D dropoff_tile, 
                            std::shared_ptr<MapObject> object);
+
+    /// allowing an object to picked up / dropped off at the same location
+    static void create_pickupable(Vec2D start_tile, Vec2D pickup_tile, std::shared_ptr<MapObject> object );
 };
