4ca277785fe6f524e420b49d12324047b60551fb
Heidi-ann <hh360@cam.ac.uk>
2014-08-21 15:58:44 +0100

    making croc a mapobject

--- src/challenges/final_challenge.cpp
+++ src/challenges/final_challenge.cpp
@@ -1,5 +1,7 @@
 #include <glog/logging.h>
 #include <memory>
+#include <thread>
+#include <chrono>
 
 #include "final_challenge.hpp"
 #include "challenge_helper.hpp"
@@ -50,32 +52,36 @@ FinalChallenge::FinalChallenge(ChallengeData *challenge_data): Challenge(challen
         return true;
     });
 
-    // TODO: fix then when joshua pushs fixes
-    // creating the crocodile
-    // LOG(INFO) << "creating croc";
-    // int croc_id = ChallengeHelper::make_object(this, "sprite/crocodile", Walkability::BLOCKED);
-    // auto croc = ObjectManager::get_instance().get_object<Object>(croc_id);
-    // if (!croc) { return; }
-
-    // // setting up python for croc
-
-    // // Register user controled sprite
-    // // Yes, this is a memory leak. Deal with it.
-    // auto properties(map->locations.at("Objects/sprite/crocodile"));
-    // auto *a_thing(new Entity(properties.location, "croc", croc_id));
-
-    // LOG(INFO) << "Registering sprite";
-    // croc->daemon = std::make_unique<LockableEntityThread>(challenge_data->interpreter->register_entity(*a_thing));
-    // LOG(INFO) << "Done!";
-
-    // croc->daemon->value->halt_soft(EntityThread::Signal::RESTART);
-    ChallengeHelper::make_sprite(
+    LOG(INFO) << "creating croc";
+    int croc_id = ChallengeHelper::make_object(
         this,
         "sprite/crocodile",
-        "final_challenge_croc",
         Walkability::BLOCKED,
         "south/still/1"
     );
+
+    auto croc = ObjectManager::get_instance().get_object<Object>(croc_id);
+    if (!croc) { return; }
+
+    // setting up python for croc
+
+    // Register user controled sprite
+    // Yes, this is a memory leak. Deal with it.
+    auto properties(map->locations.at("Objects/sprite/crocodile"));
+    auto *a_thing(new Entity(properties.location, "final_challenge_croc", croc_id));
+
+    LOG(INFO) << "Registering sprite";
+    croc->daemon = std::make_unique<LockableEntityThread>(challenge_data->interpreter->register_entity(*a_thing));
+    LOG(INFO) << "Done!";
+    //std::this_thread::sleep_for(std::chrono::seconds(4));
+    croc->daemon->value->halt_soft(EntityThread::Signal::RESTART);
+    // ChallengeHelper::make_sprite(
+    //     this,
+    //     "sprite/crocodile",
+    //     "final_challenge_croc",
+    //     Walkability::BLOCKED,
+    //     "south/still/1"
+    // );
 }
 
 void FinalChallenge::start() {
