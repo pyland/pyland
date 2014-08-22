f2f3fbb5a3afe5f9a99c30f3d4e11ff740258f70
Joshua Landau <joshua@landau.ws>
2014-08-22 02:47:34 +0100

    Better start script, as an example. No reason really.

--- src/python_embed/scripts/Ben.py
+++ src/python_embed/scripts/Ben.py
@@ -1,25 +1,40 @@
-from itertools import cycle
+"""
+This is a wall-following script written in mostly idiomatic Python.
 
+This could be used to solve mazes - potentially even very large ones,
+but may fail if starting in the wrong place on a maze with loops.
+"""
+
+# A mapping that translates a direction 90° clockwise
 rotate_cw = {
 	north: east,
-	east: south,
+	east:  south,
 	south: west,
-	west: north
+	west:  north
 }
 
+# Invert this mapping by switching the keys and values
 rotate_ccw = {v: k for k, v in rotate_cw.items()}
 
 # Get to north wall
-while move(north):
-	pass
+while walkable(north):
+	move(north)
 
-wall = north
+# Start going along the north wall,
+# walking eastward.
+direction = east
 
+# Don't stop
 while True:
-	if walkable(wall):
-		wall = rotate_ccw[wall]
+	# If the direction of the wall is walkable,
+	# you have just passed the end, so turn into it.
+	if walkable(rotate_ccw[direction]):
+		direction = rotate_ccw[direction]
 
-	if not walkable(rotate_cw[wall]):
-		wall = rotate_cw[wall]
+	# If the direction of travel is blocked, turn
+	# away from the wall.
+	while not walkable(direction):
+		direction = rotate_cw[direction]
 
-	move(rotate_cw[wall])
+	# Walk forward
+	move(direction)
