70056af2eca68a56e865ac54fdafe8e302462937
sams25 <as2464@cam.ac.uk>
2015-06-30 09:25:50 +0100

    Added the map

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define GLM_FORCE_RADIANS
+﻿#define GLM_FORCE_RADIANS
 
 #include <glog/logging.h>
 #include <boost/filesystem/operations.hpp>
@@ -456,6 +456,10 @@ Challenge* pick_challenge(ChallengeData* challenge_data) {
             challenge_data->map_name = map_name;
             challenge = new FinalChallenge(challenge_data);
             break;
+		case 4: 
+			map_name = "../maps/new_challenge.tmx";
+			challenge_data->map_name = map_name;
+            challenge = new NewChallenge(challenge_data);
         default:
             break;
     }
