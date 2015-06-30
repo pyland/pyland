a4684902d828469223a573f046500f6fb2378faa
Moosichu <readcuttingt@gmail.com>
2015-06-30 14:41:06 +0000

    Made door1 appear and the level can now be completed

--- src/challenges/new_challenge.cpp
+++ src/challenges/new_challenge.cpp
@@ -19,6 +19,7 @@ namespace py = boost::python;
 
 NewChallenge::NewChallenge(ChallengeData* _challenge_data) : Challenge(_challenge_data) {
 
+    //add the monkey to the game
     int monkey_id = ChallengeHelper::make_sprite(
         this,
         "sprite/monkey",
@@ -26,7 +27,37 @@ NewChallenge::NewChallenge(ChallengeData* _challenge_data) : Challenge(_challeng
         Walkability::BLOCKED,
         "east/still/1"
     );
-    int player = ChallengeHelper::make_sprite(this, "sprite/1","Ben", Walkability::BLOCKED, "east/still/1");
+
+    int player_id = ChallengeHelper::make_sprite(
+        this,
+        "sprite/1",
+        "Ben",
+        Walkability::BLOCKED,
+        "east/still/1"
+    );
+
+    ChallengeHelper::make_interaction("trigger/objective/finish", [this] (int) {
+        ChallengeHelper::set_completed_level(4);
+        finish();
+        return false;
+    });
+
+    //trigger/objective/button1
+    //trigger/objective/button4
+    //trigger/objective/door1
+    //The following code is for door 1 and the buttons that activate it.
+    int door_1_id = ChallengeHelper::make_object(this, "trigger/objective/door1", Walkability::BLOCKED, "4");
+
+    ChallengeHelper::make_interaction("trigger/objective/button1", [this] (int) {
+
+        return true;
+    });
+
+    ChallengeHelper::make_interaction("trigger/objective/button4", [this] (int) {
+
+        return true;
+    });
+
 }
 
 
@@ -36,8 +67,8 @@ NewChallenge::~NewChallenge() {
 
 void NewChallenge::start() {
 Engine::print_dialogue ( "Tom",
-		"Welcome to my new level\n"
-	);
+        "Welcome to my new level\n"
+    );
 }
 
 void NewChallenge::finish() {
