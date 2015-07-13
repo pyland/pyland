d665b4788095d38b7744178bd31cb53ace542e60
Tom Read-Cutting <tr395@cam.ac.uk>
2015-07-13 15:26:08 +0100

    Some minor challenge changes

--- src/python_embed/scripts/moving_fruit_challenge_fruit.py
+++ src/python_embed/scripts/moving_fruit_challenge_fruit.py
@@ -8,33 +8,33 @@ from random import randint
 fruits = {}
 
 def basket():
-	return {"x": object_basket.x, "y": object_basket.y}
+    return {"x": object_basket.x, "y": object_basket.y}
 
 def move_fruit(object, x, y)
-	me = {"x": me.x, "y": me.y}
-	if me.x - basket.x < 0:
+    me = {"x": me.x, "y": me.y}
+    if me.x - basket.x < 0:
                 try:
                         move_west()
                 except:
-			pass
-	if me.x - basket.x > 0:
+            pass
+    if me.x - basket.x > 0:
                 try:
                         move_east()
                 except:
-			pass
-	if me.y - basket.y < 0:
+            pass
+    if me.y - basket.y < 0:
                 try:
                         move_south()
                 except:
-			pass
+            pass
         if me.y - basket.y > 0:
-		try:	
-			move_north()
-		except:
-			pass
-	else:
-		while move(randint(0,1),randint(0,1)) != True:
-			move(randint(0,1),randint(0,1))
+        try:	
+            move_north()
+        except:
+            pass
+    else:
+        while move(randint(0,1),randint(0,1)) != True:
+            move(randint(0,1),randint(0,1))
 
 while True:
-	move_fruit()
+    move_fruit()
