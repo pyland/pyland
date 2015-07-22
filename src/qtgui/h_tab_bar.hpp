3dd88de32495a0f890494b1e45c55e9bde3d0cde
alexandaday <alexandaday@gmail.com>
2015-07-22 09:55:39 +0000

    Fix tab widget issue

--- src/qtgui/h_tab_bar.hpp
+++ src/qtgui/h_tab_bar.hpp
@@ -3,23 +3,14 @@
 
 #include<QStylePainter>
 
-class QHTabWidget : public QTabWidget
-{
-public:
-    QHTabWidget() : QTabWidget() {
-        setTabBar(new QHTabBar());
-    }
-
-};
-
 class QHTabBar : public QTabBar
 {
 private:
     QSize tabSizeHint(int) const {
         return QSize(25, 36);
     }
-    void paintEvent(*QPaintEvent) {
-        QStylePainter stylePainter = new QStylePainter(this);
+    void paintEvent(QPaintEvent *) {
+        QStylePainter stylePainter(this);
         for (int i = 0; i < count(); i++) {
             QStyleOptionTabV3 tabStyleOption;
             initStyleOption(&tabStyleOption,i);
@@ -35,6 +26,15 @@ private:
             tabPainter.drawText(curRect, Qt::AlignCenter, tabText);
             tabPainter.end();
         }
+    }
+
+};
+
+class QHTabWidget : public QTabWidget
+{
+public:
+    QHTabWidget() : QTabWidget() {
+        setTabBar(new QHTabBar());
     }
 
 };
