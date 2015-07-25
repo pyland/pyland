050022661948729bed6476246422dc781aa37c53
Tom Read-Cutting <tr395@cam.ac.uk>
2015-07-25 20:09:44 +0100

    Cleaned up and commented python code, only refactored, no functional changes.

---
+++ game/components/scoped_interpreter.py
@@ -0,0 +1,60 @@
+import code
+import sys
+import io
+import contextlib
+
+""" TODO: CLEAN THIS UP, THIS UP A LOT (do the same in bootsrapper.py), this is a really really really
+weird, and as far as I can see, unnesecary way of defining a class. But what do I know :P
+TODO: Make it so that the scoped_intepreter in bootstrapper.py is pulled from here.
+
+Creates and defines a class called ScopedIntepreter which is used to run the player-code.
+"""
+class ScopedInterpreter(code.InteractiveInterpreter):
+
+    """ Creates a scoped intepreter which can be used to run some code which is provided as the argument to runcode()
+    imbued_locals -- A dictionary from strings to objects, each object will be available to the code being run in the interpreter under the name given by their dictionary key
+    output_reader -- A function which takes a string as an argument, any output from the scoped code will be passed to here: TODO: work out if this is what it actually does!
+    """
+    def __init__(self, imbued_locals, output_reader):
+        super().__init__(imbued_locals)
+
+    """ TODO: work out what this does, was in bootstrapper.py so assumed to be needed here!"""
+    def write(self, data):
+        output_reader(data)
+
+    def runcode(self, code):
+        old_stdout = sys.stdout
+        sys.stdout = io.TextIOWrapper(io.BytesIO(), sys.stdout.encoding)
+
+        #"""TODO: work out how to catch exceptions safely and handle them with python,
+        #then we can query the database to print output and errors messages cleanly to the user!"""
+        #TODO: work out what the hell contextlib.closing does, don't forget to change bootstrapper.py based on that!
+        try:
+            with contextlib.closing(sys.stdout):
+                pass
+                # Emulate super().runcode(code), but don't catch
+                # RESTART, STOP or KILL because those should
+                # propogate to allow thread control.
+                #
+                # The code here is a trivial modification of the
+                # source of the implementaion we're overloading.
+    
+                try:
+                    exec(code, self.locals)
+                except (SystemExit): #, RESTART, STOP, KILL, TODO: work out what system_exit actually is!
+                    raise
+                except:
+                    self.showtraceback() # TODO: work out how this works, comment it in bootstrapper.py as well
+                # Read
+                sys.stdout.seek(0)
+                output = sys.stdout.read()
+    
+        finally:
+            sys.stdout = old_stdout
+    
+        if output:
+            """ This if statement is called whenever user calls "print(data)" inside their code,
+            TODO: make it so that it prints the output to the player console! However, this version
+            only prints to the console once the script has finished as opposed to printing in real time?
+            """
+            output_reader(output)
--- game/objects/characters/player/scriptrunner.py
+++ game/objects/characters/player/scriptrunner.py
@@ -6,6 +6,9 @@ import contextlib
 import time
 import threading
 
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/../../../components')
+from scoped_interpreter import ScopedInterpreter
+
 """ This file contains all the implementation details of how player scripts are interpreted and run.
 """
 
@@ -13,9 +16,25 @@ import threading
 """
 def start(game_object, script_name):
     print("Running Player Script: " + script_name) #TODO: change this to some kind of script debug.
+
+    """ imbued_locals is a python dictionary of python objects (variables, methods, class instances etc.)
+    available to the player. :)
+    eg. if there is an entry named "fart" whos entry is blob, then in the level script, any reference to fart
+    will be refering to what blob is known as here.
+    Here the list of game_objects is being looped through, and their names are being mapped to each instance :)
+    """
+    imbued_locals = {}
+
+    # Provide all the movement functions to the player, but make them blocking.
+    imbued_locals["move_north"] = make_blocking(game_object.move_north)
+    imbued_locals["move_east"] = make_blocking(game_object.move_east)
+    imbued_locals["move_south"] = make_blocking(game_object.move_south)
+    imbued_locals["move_west"] = make_blocking(game_object.move_west)
+
+    
     #Creates 
-    ScopedInterpreter = create_execution_scope(game_object) #Get the class definition for the ScopedIntepreter
-    scoped_interpreter = ScopedInterpreter() #create an instance of it
+    #ScopedInterpreter = create_execution_scope(game_object) #Get the class definition for the ScopedIntepreter
+    scoped_interpreter = ScopedInterpreter(imbued_locals, print) #create an instance of it
     script_filename = os.path.dirname(os.path.realpath(__file__)) + "/../../../player_scripts/" + script_name + ".py"; #grab the absolute location of the script TODO: implement this path stuff in a config (ini) file!!!!!
     
     #open and print the script
@@ -53,72 +72,4 @@ def make_blocking(async_function):
         return
     
     return blocking_function
-    
-
-""" TODO: CLEAN THIS UP, THIS UP A LOT (do the same in bootsrapper.py), this is a really really really
-weird, and as far as I can see, unnesecary way of defining a class. But what do I know :P
-
-Creates and defines a class called ScopedIntepreter which is used to run the player-code.
-"""
-def create_execution_scope(game_object): #""", RESTART, STOP, KILL"""
-    """ imbued_locals is a python dictionary of python objects (variables, methods, class instances etc.)
-    available to the player. :)
-    eg. if there is an entry named "fart" whos entry is blob, then in the level script, any reference to fart
-    will be refering to what blob is known as here.
-    Here the list of game_objects is being looped through, and their names are being mapped to each instance :)
-    """
-    imbued_locals = {}
-
-    # Provide all the movement functions to the player, but make them blocking.
-    imbued_locals["move_north"] = make_blocking(game_object.move_north)
-    imbued_locals["move_east"] = make_blocking(game_object.move_east)
-    imbued_locals["move_south"] = make_blocking(game_object.move_south)
-    imbued_locals["move_west"] = make_blocking(game_object.move_west)
-
-    class ScopedInterpreter(code.InteractiveInterpreter):
-        def __init__(self):
-            super().__init__(imbued_locals)
-
-        """ TODO: work out what this does, was in bootstrapper.py so assumed to be needed here!"""
-        def write(self, data):
-            print(data)
-
-        def runcode(self, code):
-            old_stdout = sys.stdout
-            sys.stdout = io.TextIOWrapper(io.BytesIO(), sys.stdout.encoding)
-
-            #"""TODO: work out how to catch exceptions safely and handle them with python,
-            #then we can query the database to print output and errors messages cleanly to the user!"""
-            #TODO: work out what the hell contextlib.closing does, don't forget to change bootstrapper.py based on that!
-            try:
-                with contextlib.closing(sys.stdout):
-                    pass
-                    # Emulate super().runcode(code), but don't catch
-                    # RESTART, STOP or KILL because those should
-                    # propogate to allow thread control.
-                    #
-                    # The code here is a trivial modification of the
-                    # source of the implementaion we're overloading.
-            
-                    try:
-                        exec(code, self.locals)
-                    except (SystemExit): #, RESTART, STOP, KILL, TODO: work out what system_exit actually is!
-                        raise
-                    except:
-                        self.showtraceback() # TODO: work out how this works, comment it in bootstrapper.py as well
-                    # Read
-                    sys.stdout.seek(0)
-                    output = sys.stdout.read()
-            
-            finally:
-                sys.stdout = old_stdout
-            
-            if output:
-                """ This if statement is called whenever user calls "print(data)" inside their code,
-                TODO: make it so that it prints the output to the player console! However, this version
-                only prints to the console once the script has finished as opposed to printing in real time?
-                """
-                print(output)
-
-    return ScopedInterpreter
     
