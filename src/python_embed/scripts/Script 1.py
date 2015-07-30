63c6694476d2f8ab4ee3084c0505b09f581e822f
Tom Read-Cutting <tr395@cam.ac.uk>
2015-07-30 15:51:56 +0100

    Fixed Ubuntu errors and added helpful message to script 1.py :)

--- src/python_embed/scripts/Script 1.py
+++ src/python_embed/scripts/Script 1.py
@@ -1,3 +1,5 @@
-move_east()
-move_north()
-#hi tom
+"""
+This is where you will run python scripts to help
+you complete the game! Read the helpful messages
+in the game and they will tell you what to do! :)
+"""
