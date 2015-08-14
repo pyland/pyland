fda18b9f6462b19f7d9e88f749c2e3f067c5552a
Tom Read-Cutting <tr395@cam.ac.uk>
2015-08-14 09:49:16 +0100

    Passing named parameters as callbacks to show_dialogue in world one's levels

--- game/levels/world_one/level_two/main/scripts/start.py
+++ game/levels/world_one/level_two/main/scripts/start.py
@@ -9,17 +9,17 @@ player_one.face_east()
 
 dialogue_sequence = [
     lambda callback: player_one.set_busy(True, callback = callback),
-    lambda callback: engine.show_dialogue("Welcome to level two!", callback),
-    lambda callback: engine.show_dialogue("Walk to the first checkpoint over there!", callback),
+    lambda callback: engine.show_dialogue("Welcome to level two!", callback = callback),
+    lambda callback: engine.show_dialogue("Walk to the first checkpoint over there!", callback = callback),
     lambda callback: player_one.set_busy(False, callback = callback)
 ]
 
 
 dialogue_checkpoint_one = [
     lambda callback: player_one.set_busy(True, callback = callback),
-    lambda callback: engine.show_dialogue("Great! But we can also walk by writing code.", callback),
-    lambda callback: engine.show_dialogue("A script will appear in the PySripter, press 'run' to see what happens once it has!", callback),
-    lambda callback: engine.show_dialogue("I'll meet you at the next checkpoint!", callback),
+    lambda callback: engine.show_dialogue("Great! But we can also walk by writing code.", callback = callback),
+    lambda callback: engine.show_dialogue("A script will appear in the PySripter, press 'run' to see what happens once it has!", callback = callback),
+    lambda callback: engine.show_dialogue("I'll meet you at the next checkpoint!", callback = callback),
     lambda callback: player_one.set_busy(False, callback = callback),
     lambda callback: engine.clear_scripter(callback = callback),
     lambda callback: engine.insert_to_scripter("move_east()", callback = callback)
@@ -27,18 +27,18 @@ dialogue_checkpoint_one = [
 
 dialogue_checkpoint_two = [
     lambda callback: player_one.set_busy(True, callback = callback),
-    lambda callback: engine.show_dialogue("Try typing move_east() into the codeblock many times. Make sure they are on separate lines! So you can run a big block at once!", callback),
-    lambda callback: engine.show_dialogue("If you press on the \"speed\" button in the very bottom right-hand corner of the screen, you will be able to run your code super-fast.", callback),
-    lambda callback: engine.show_dialogue("I'll meet you at the end!", callback),
+    lambda callback: engine.show_dialogue("Try typing move_east() into the codeblock many times. Make sure they are on separate lines! So you can run a big block at once!", callback = callback),
+    lambda callback: engine.show_dialogue("If you press on the \"speed\" button in the very bottom right-hand corner of the screen, you will be able to run your code super-fast.", callback = callback),
+    lambda callback: engine.show_dialogue("I'll meet you at the end!", callback = callback),
     lambda callback: player_one.set_busy(False, callback = callback)
 ]
 
 dialogue_checkpoint_end = [
     lambda callback: player_one.set_busy(True, callback = callback),
-    lambda callback: engine.show_dialogue("You've reached the end!", callback),
-    lambda callback: engine.show_dialogue("We can also move in other directions by typing in move_north(), move_west(), and move_south() into the codeblock.", callback),
-    lambda callback: engine.show_dialogue("The run button runs all the current code in the codeblock and you can change it to do whatever you want!", callback),
-    lambda callback: engine.show_dialogue("What do you call a snake that likes to program?", callback),
+    lambda callback: engine.show_dialogue("You've reached the end!", callback = callback),
+    lambda callback: engine.show_dialogue("We can also move in other directions by typing in move_north(), move_west(), and move_south() into the codeblock.", callback = callback),
+    lambda callback: engine.show_dialogue("The run button runs all the current code in the codeblock and you can change it to do whatever you want!", callback = callback),
+    lambda callback: engine.show_dialogue("What do you call a snake that likes to program?", callback = callback),
     lambda callback: engine.show_dialogue("A code-bra!", callback),
     lambda callback: player_one.set_busy(False, callback = callback)
 
