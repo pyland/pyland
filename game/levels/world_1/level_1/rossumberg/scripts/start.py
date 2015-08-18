22c049e79d090b22b6ab2b7f397094512342e784
Tom Read-Cutting <tr395@cam.ac.uk>
2015-08-18 18:09:07 +0100

    found issue for Anindya to debug

--- game/levels/world_1/level_1/rossumberg/scripts/start.py
+++ game/levels/world_1/level_1/rossumberg/scripts/start.py
@@ -2,10 +2,11 @@ player_one.focus()
 
 engine.play_music("calm")
 engine.set_ui_colours((200,255,200),(215,255,215)) #TODO: save these colours in the config.
-"""
+
 dialogue_sequence = [
     lambda callback: player_one.set_busy(True, callback = callback),
     lambda callback: player_one.face_south(callback = callback),
+    lambda callback: engine.show_dialogue("afasfasfsfasfaf!!!!", callback = callback),
     lambda callback: heidi.face_west(callback = callback),
     lambda callback: engine.show_dialogue("%player_name%!!!!", callback = callback),
     lambda callback: heidi.move_west(callback = callback),
@@ -18,13 +19,31 @@ dialogue_sequence = [
     lambda callback: player_one.face_east(callback = callback),
     lambda callback: engine.show_dialogue("Today is the big day! You did remember right?", callback = callback),
     #TODO: change the level to the intro level
+    lambda callback: heidi.move_east(callback = callback),
+    lambda callback: heidi.move_east(callback = callback),
+    lambda callback: heidi.move_east(callback = callback),
+    lambda callback: engine.show_dialogue("TESTSTSSTSTSTSTSTSTT", callback = callback),
+    lambda callback: heidi.move_east(callback = callback),
+    lambda callback: heidi.move_east(callback = callback),
     lambda callback: player_one.set_busy(False, callback = callback)
 ]
-
-engine.run_callback_list_sequence(dialogue_sequence, lambda: engine.show_dialogue_with_options("Broo", {"south": lambda : player_one.move_south(), "printj" : lambda : engine.print_terminal("j")}))
 """
-
-engine.show_dialogue_with_options("Broo",{
-    "south": lambda : player_one.move_south(),
-    "printj" : lambda : engine.print_terminal("j")
-})
+engine.run_callback_list_sequence(
+    dialogue_sequence,
+    lambda: engine.show_dialogue_with_options(
+        "Broo",
+        {
+            "south": lambda : player_one.move_south(),
+            "printj" : lambda : engine.print_terminal("j")
+        }
+    )
+) """
+"""
+engine.run_callback_list_sequence(
+    dialogue_sequence
+)
+"""
+engine.run_callback_list_sequence(
+    dialogue_sequence,
+    heidi.move_west
+)
