7fb9d6e0643bc9e06e6b3b80ef9d77b3385e4257
Tom Read-Cutting <tr395@cam.ac.uk>
2015-08-18 18:33:26 +0100

    Added first bits of dialogue to first level

--- game/levels/world_1/level_1/rossumberg/scripts/start.py
+++ game/levels/world_1/level_1/rossumberg/scripts/start.py
@@ -6,7 +6,6 @@ engine.set_ui_colours((200,255,200),(215,255,215)) #TODO: save these colours in 
 dialogue_sequence = [
     lambda callback: player_one.set_busy(True, callback = callback),
     lambda callback: player_one.face_south(callback = callback),
-    lambda callback: engine.show_dialogue("afasfasfsfasfaf!!!!", callback = callback),
     lambda callback: heidi.face_west(callback = callback),
     lambda callback: engine.show_dialogue("%player_name%!!!!", callback = callback),
     lambda callback: heidi.move_west(callback = callback),
@@ -17,33 +16,22 @@ dialogue_sequence = [
     lambda callback: heidi.move_west(callback = callback),
     lambda callback: heidi.move_west(callback = callback),
     lambda callback: player_one.face_east(callback = callback),
-    lambda callback: engine.show_dialogue("Today is the big day! You did remember right?", callback = callback),
-    #TODO: change the level to the intro level
-    lambda callback: heidi.move_east(callback = callback),
-    lambda callback: heidi.move_east(callback = callback),
-    lambda callback: heidi.move_east(callback = callback),
-    lambda callback: engine.show_dialogue("TESTSTSSTSTSTSTSTSTT", callback = callback),
-    lambda callback: heidi.move_east(callback = callback),
-    lambda callback: heidi.move_east(callback = callback),
-    lambda callback: player_one.set_busy(False, callback = callback)
 ]
-"""
+
 engine.run_callback_list_sequence(
     dialogue_sequence,
     lambda: engine.show_dialogue_with_options(
-        "Broo",
+        "Today is the big day! You did remember right?",
         {
-            "south": lambda : player_one.move_south(),
-            "printj" : lambda : engine.print_terminal("j")
+            "Yes": if_remembered_big_day,
+            "No": lambda : player_one.move_north(lambda: player_one.set_busy(False, callback = callback)),
         }
     )
-) """
-"""
-engine.run_callback_list_sequence(
-    dialogue_sequence
 )
-"""
-engine.run_callback_list_sequence(
-    dialogue_sequence,
-    heidi.move_west
-)
+
+def if_remembered_big_day():
+    sequence = [
+        lambda callback: engine.show_dialogue("I thought you would!", callback = callback),
+        lambda callback: player_one.set_busy(False, callback = callback),
+    ]
+    engine.run_callback_list_sequence(sequence)
