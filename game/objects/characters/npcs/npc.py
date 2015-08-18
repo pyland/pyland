549cac8775bd83f52e8df388e358ffd7c109862e
Tom Read-Cutting <tr395@cam.ac.uk>
2015-08-18 14:53:52 +0100

    Properly integrated Ying's new stuff with Pyland and added heidi as a character

---
+++ game/objects/characters/npcs/npc.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/..')
+from character import Character
+
+""" A brief description of the class will go here.
+The auto-generated comment produced by the script should also mention where to get a list of the api
+and which built-in variables exist already.
+"""
+class NPC(Character):
+
+    def initialise(self):
+        super().initialise()
+
+    def player_action(self, player_object):
+        """ This is the method that is run if the player presses the action key while facing a character.
+        
+        TODO: move this to character.py and make it print an explenation on how to override this by default.
+        """
+        pass
