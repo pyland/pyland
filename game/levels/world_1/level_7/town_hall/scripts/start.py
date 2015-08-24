072d8ae1244f74a4b3add9c8d44e1c15b3ae6f0c
Alex Day <alexandaday@gmail.com>
2015-08-24 21:28:24 +0100

    Can run callbacks after character is finished running script

--- game/levels/world_1/level_7/town_hall/scripts/start.py
+++ game/levels/world_1/level_7/town_hall/scripts/start.py
@@ -3,7 +3,7 @@ from random import randint
 player.focus()
 engine.update_player_name(engine.get_player_name(), player.get_focus_button_id())
 
-engine.play_music("world_1_jungle")
+engine.play_music("eery")
 engine.update_level("7")
 engine.set_py_tabs(9)
 
@@ -13,6 +13,14 @@ myla.face_east()
 
 leader.face_west()
 
+snakes = [snake1,snake2,snake3,snake4,snake5,snake6,snake7]
+snakes_initial_pos = []
+snakes_final_pos = []
+
+for snake in snakes:
+    snakes_initial_pos.append((0,0))
+    snakes_final_pos.append((0,0))
+
 snake1.face_east()
 snake2.face_east()
 snake3.face_east()
@@ -21,13 +29,14 @@ snake5.face_east()
 snake6.face_east()
 snake7.face_east()
 
-snake1.change_state("orange")
-snake2.change_state("green")
-snake3.change_state("red")
-snake4.change_state("green")
-snake5.change_state("red")
-snake6.change_state("yellow")
-snake7.change_state("orange")
+snakes[0].change_state("orange")
+snakes[1].change_state("green")
+snakes[2].change_state("red")
+snakes[3].change_state("green")
+snakes[4].change_state("red")
+snakes[5].change_state("yellow")
+snakes[6].change_state("orange")
+
 leader.change_state("blue")
 
 #Store whether you have correctly helped each player yet
@@ -38,6 +47,9 @@ for x in range(7):
 def update_snake_stage(snake = 0, stage = 1, callback = lambda: None):
     snake_helped[snake] = stage
     callback()
+
+def update_snake_final_position(snake_index):
+    snakes_final_pos[snake_index] = snakes[snake_index].get_position()
 
 def pace(cur_object, callback = lambda: None):
     cur_object.move_north(lambda: cur_object.wait(1.0, lambda: cur_object.move_south(lambda: cur_object.move_south(lambda: cur_object.wait(1.0, lambda: cur_object.move_north(lambda: pace(cur_object)))))))
@@ -71,13 +83,13 @@ dialogue_sequence = [
 ]
 
 player1_sequence = [
-    #lambda callback: player.set_busy(True, callback = callback),
+    lambda callback: player.set_busy(True, callback = callback),
     lambda callback: snake1.stop_turning(callback = callback),
     lambda callback: snake1.turn_to_face(player, callback = callback),
     lambda callback: engine.show_dialogue("I'm scared. I thought the catchers were just a myth.", callback = callback),
     #lambda callback: engine.show_dialogue("My PyRunner script is broken. Can you fix it so I move east and get to the desert?", callback = callback),
     lambda callback: engine.show_dialogue_with_options(
-        "My PyRunner script is broken. Can you fix it so I can move east fast enough to escape to the desert?",
+        "My PyRunner script is broken. Can you fix it so I can use it to run move_east() and escape fast enough to the desert?",
         options = {
             "Yes": lambda: engine.run_callback_list_sequence(player1_help_sequence),
             "No" : lambda: engine.run_callback_list_sequence(player1_reject_sequence)
@@ -89,7 +101,7 @@ player1_sequence = [
 
 player1_help_sequence = [
     lambda callback: engine.show_dialogue("Thank you! Here is my script.",  callback = callback),
-    #lambda callback: player.set_busy(True, callback = callback),
+    lambda callback: player.set_busy(True, callback = callback),
     lambda callback: update_snake_stage(snake = 0, stage = 1, callback = callback),
     lambda callback: engine.show_external_script(
         confirm_callback = lambda: engine.run_callback_list_sequence(player1_try_script_sequence),
@@ -102,24 +114,46 @@ player1_help_sequence = [
 ]
 
 player1_reject_sequence = [
-    lambda callback: engine.show_dialogue("Okay then...", callback = callback),
+    lambda callback: engine.show_dialogue("I didn't think you'd be able to.", callback = callback),
+    lambda callback: snake1.start_turning(time = 0.1, frequency = 8, callback = callback),
+    lambda callback: player.set_busy(False, callback = callback),
     #lambda callback: engine.wait(5.0, callback = callback),
     #lambda callback: player.set_busy(False, callback = callback),
 ]
 
+def snake_finish_script(snake_index):
+
+    update_snake_final_position(snake_index)
+
+    engine.print_terminal(initialPosition[0])
+    engine.print_terminal(initialPosition[1])
+    snake1.run_script(script_to_run = 10, callback = snake_finish_script(0))#callback = lambda: engine.show_dialogue("Done with script!."))
+    finalPosition = snake1.get_position()
+    engine.print_terminal(finalPosition[0])
+    engine.print_terminal(finalPosition[1])
+
+def player1_run_script(callback = None):
+        #initialPosition = snake1.get_position()
+        #snakes_initial_pos[0] = initialPosition
+        engine.print_terminal(initialPosition[0])
+        engine.print_terminal(initialPosition[1])
+        snake1.run_script(script_to_run = 10, callback = snake_finish_script(0))#callback = lambda: engine.show_dialogue("Done with script!."))
+        finalPosition = snake1.get_position()
+        engine.print_terminal(finalPosition[0])
+        engine.print_terminal(finalPosition[1])
+        callback()
 
 player1_try_script_sequence = [
-    lambda callback: engine.show_dialogue("Thanks for the help, running now", callback = callback),
+    lambda callback: engine.show_dialogue("Thank you, it means a lot! I'll try running it now.", callback = callback),
     #lambda callback: player.set_busy(False, callback = callback),
     lambda callback: update_snake_stage(snake = 0, stage = 0, callback = callback),
     lambda callback: player.set_busy(False, callback = callback),
-    lambda callback: snake1.run_script(script_to_run = 10),
+    lambda callback: player1_run_script(),
     #lambda callback: engine.print_terminal(engine.get_external_script()),
 ]
 
 player1_cancel_script_sequence = [
-    #lambda callback: player.set_busy(False, callback = callback),
-    lambda callback: engine.show_dialogue("You cancelled on me!", callback = callback),
+    lambda callback: player.set_busy(False, callback = callback),
 ]
 
 
