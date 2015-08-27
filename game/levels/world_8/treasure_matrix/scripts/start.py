67c351b5a2a081241b323730233d6471e7604bc8
sams25 <as2464@cam.ac.uk>
2015-08-27 16:05:10 +0100

    Got move_west and move_north disabled

--- game/levels/world_8/treasure_matrix/scripts/start.py
+++ game/levels/world_8/treasure_matrix/scripts/start.py
@@ -9,13 +9,13 @@ engine.set_py_tabs(2)
 #overwrite the move_west and move_north functions for the player so that the player cannot move up the ledges
 
 def move_west(callback = lambda: None):
-    self.face_west()
-    self.move_on_spot(callback)
+    player_one.face_west()
+    player_one.move_on_spot(callback)
 
 def move_north(callback = lambda: None):
-    self.face_north()
-    self.move_on_spot(callback)
+    player_one.face_north()
+    player_one.move_on_spot(callback)
 
-player_one.move_north = move_north
+player_one.move_north =  move_north
 player_one.move_west = move_west
 
