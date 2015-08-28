64da5b5b5b49fed0e8ffbd88583ecb48e9659a9e
alexandaday <alexandaday@gmail.com>
2015-08-28 10:20:37 +0000

    Updating template level

--- game/levels/Template/scripts/start.py
+++
@@ -1,1 +0,0 @@
-#Implement your level logic here!
---
+++ game/levels/template/scripts/start.py
@@ -0,0 +1,7 @@
+#Implement your level logic here!
+
+player.focus()
+
+player.set_character_name("PyPlayer")
+
+engine.play_music("world_1_jungle")
