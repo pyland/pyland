0a47fc2ba5edbfac579066102667bed7c46328b4
Tom Read-Cutting <tr395@cam.ac.uk>
2015-09-10 15:02:40 +0100

    level3 so that the outline of what the level will do is in the level code

--- game/levels/world1/level3/main/scripts/start.py
+++ game/levels/world1/level3/main/scripts/start.py
@@ -40,15 +40,9 @@ def myla_introduces_programming_action(player_object):
                 lambda callback: engine.show_dialogue("Awesome!", callback = callback), #TODO: add all the possible reactions to what the player can give Myla
             ], callback = callback),
             cancel_callback = lambda: engine.run_callback_list_sequence([
-                lambda callback: engine.show_dialogue("Oh, you've forgotten my name already? I'm Myla! Try giving it another go.", callback = callback),
+                lambda callback: engine.show_dialogue("I would at least give it to me to run! TODO: go back to running", callback = callback),
             ], callback = callback),
         ),
-        lambda callback: engine.show_dialogue("print() is an example of something called a function.", callback = callback),
-        lambda callback: engine.show_dialogue("A function takes what you give it in between the brackets, and does something with it!", callback = callback),
-        lambda callback: engine.show_dialogue("The thing you give to a function is called an argument.", callback = callback),
-        lambda callback: engine.show_dialogue("If it's a lot to take in, don't worry, we will back to all of this again and again in many different ways.", callback = callback),
-        lambda callback: engine.show_dialogue("In summary, you just used a function called print, and gave it my name as an argument.", callback = callback),
-        lambda callback: engine.show_dialogue("Anyway, let's get moving, I will teach you some more in a bit.", callback = callback),
         lambda callback: player_one.set_busy(False, callback = callback),
     ])
 
@@ -59,23 +53,23 @@ def myla_introduces_variables_action(player_object):
         lambda callback: player_one.set_busy(True, callback = callback),
         lambda callback: player_one.face_east(callback = callback),
         lambda callback: engine.show_dialogue("Now I'm going to teach you how to write your own function.", callback = callback),
+        lambda callback: engine.show_dialogue("Functions are really, really, really useful. As you can write some code, and re-use it in many different situations!", callback = callback),
         lambda callback: engine.show_external_script(
-            external_dialogue = "Try printing your name this time! However, do it by changing what name is equals to, don't give it straight to print!",
+            external_dialogue = "Here is an example function...",
             script_init = lambda: engine.run_callback_list_sequence([
                 lambda callback: engine.clear_scripter(callback = callback),
-                lambda callback: engine.insert_to_scripter("name = \"Myla\"\n", callback = callback),
-                lambda callback: engine.insert_to_scripter("print(name)", callback = callback), #TODO, add all the reactions to what the player can give Myla
+                lambda callback: engine.insert_to_scripter("def print_hello(name):\n", callback = callback),
+                lambda callback: engine.insert_to_scripter("  print(\"Hello \" + name + \"!\")\n", callback = callback),
+                lambda callback: engine.insert_to_scripter("print_hello('Myla')", callback = callback), #TODO, add all the reactions to what the player can give Myla
             ]),
             confirm_callback = lambda: engine.run_callback_list_sequence([
                 lambda callback: engine.show_dialogue("Awesome!", callback = callback),
             ], callback = callback),
             cancel_callback = lambda: engine.run_callback_list_sequence([
-                lambda callback: engine.show_dialogue("Oh, you've forgotten your own name already? Hahaha, you're called " + engine.get_player_name() + " silly.", callback = callback),
+                lambda callback: engine.show_dialogue("Here is the printing section.", callback = callback),
             ], callback = callback)
         ),
         lambda callback: engine.show_dialogue("Well done " + engine.get_player_name() + "!", callback = callback),
-        lambda callback: engine.show_dialogue("Variables are really useful for making your program remember things, they will really come in handy for when we do maths.", callback = callback),
-        lambda callback: engine.show_dialogue("Don't worry if you don't like doing maths much, the thing that's useful about programming is that the PyScripter does all the maths for you!", callback = callback),
         lambda callback: player_one.set_busy(False, callback = callback),
     ])
 
@@ -85,7 +79,7 @@ myla_introduces_variables.player_walked_on = myla_introduces_variables_action
 #setting the player's starting position
 engine.run_callback_list_sequence([
     lambda callback: player_one.move_to(exit_to_world.get_position(), callback = callback),
-    lambda callback: player_one.move_north(callback = callback),
+    lambda callback: player_one.move_west(callback = callback),
 ])
 myla_start_position = exit_to_world.get_position()
 #end setting the player's starting position
@@ -111,13 +105,11 @@ myla.follow(player_one)
 player_one.focus()
 engine.play_music("world_1_jungle")
 
-toll_person.set_dialogue_list([
-    "This person will want to toll you to cross the bridge, before letting you through",
-    "However, their PyRunner doesn't work and Myla cheekily tells you to 'fix' it so that...",
-    "...it always show that everyone has payed.",
-    "That will resolve and complete the level. The script the toll_person has will be to the effect of...",
-    "amount_payed = \"£0\" \n print(amount_payed)",
-    "The played can replace the amount_payed with whatever they want above the charge of £10.",
-    "That will complete the level",
+card_person.face_south()
+card_person.set_dialogue_list([
+    "This person will have a PyRunner that automatically printed their special occasion cards for them.",
+    "Here the player will have to write a function for them to do that.",
+    "They will be given a broken version of the function and have to fix it.",
+    "The fun but is, the player can make the function print whatever they want it to!",
 ])
 
